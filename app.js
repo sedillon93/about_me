8d17bf464caca83cabbc3e6f3b66a507bf2a8a86
Shannon Dillon Guha <sedillon93@gmail.com>
2017-08-08 13:59:27 -0700

    create variable and prompts

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 'use:strict';
+
+var seattle = prompt('I was born in Washington state');
+var major = prompt('In college I majored in chemistry');
+var german = prompt('I spent five years studying German in school');
+var college = prompt('I went to Colgate University in central New York');
+var scuba = prompt('Scuba diving is one of my favorite activities');
