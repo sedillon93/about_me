8b7006577712759fdcf1264230d7691d4627ff47
Shannon Dillon Guha <sedillon93@gmail.com>
2017-08-08 14:22:10 -0700

    add first alert message using if/else conditional

--- app.js
+++ app.js
@@ -2,11 +2,21 @@
 
 var seattle = prompt('I was born in Washington state');
 consolge.log('I was born in WA state');
+if (seattle = y.toLowerCase() || yes.toLowerCase()) {
+  alert('Good try! But the answer if no');
+}
+else {
+  alert('Good job! You got it right!');
+}
+
 var major = prompt('In college I majored in chemistry');
 console.log('I majored in chemistry');
+
 var german = prompt('I spent five years studying German in school');
 console.log('I studied German for 5 years');
+
 var college = prompt('I went to Colgate University in central New York');
 console.log('I went to Colgate University');
+
 var scuba = prompt('Scuba diving is one of my favorite activities');
 console.log('Scuba diving is my favorite activity');
