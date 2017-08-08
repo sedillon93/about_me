f9e9e35407113b227d865c287504b4809066d45b
Shannon Dillon Guha <sedillon93@gmail.com>
2017-08-08 14:28:49 -0700

    add if/else statements to remaining four question prompts

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use:strict';
 
-var seattle = prompt('I was born in Washington state').toLowerCase;
+var seattle = prompt('I was born in Washington state').toLowerCase();
 console.log('I was born in WA state');
 if (seattle = 'y' || 'yes') {
   alert('Good try! But the answer if no');
@@ -9,14 +9,38 @@ else {
   alert('Good job! You got it right!');
 }
 
-var major = prompt('In college I majored in chemistry');
+var major = prompt('In college I majored in chemistry').toLowerCase();
 console.log('I majored in chemistry');
+if (major = 'y' || 'yes') {
+  alert('Good try! But the answer if no');
+}
+else {
+  alert('Good job! You got it right!');
+}
 
-var german = prompt('I spent five years studying German in school');
+var german = prompt('I spent five years studying German in school').toLowerCase();
 console.log('I studied German for 5 years');
+if (german = 'y' || 'yes') {
+  alert('High five! You got it!');
+}
+else {
+  alert('So close! But the answer is yes');
+}
 
-var college = prompt('I went to Colgate University in central New York');
+var college = prompt('I went to Colgate University in central New York').toLowerCase();
 console.log('I went to Colgate University');
+if (college = 'y' || 'yes') {
+  alert('Well done! You\'re doing really well');
+}
+else {
+  alert('Sorry, you missed that one');
+}
 
 var scuba = prompt('Scuba diving is one of my favorite activities');
 console.log('Scuba diving is my favorite activity');
+if (scuba = 'y' || 'yes') {
+  alert('Wow! You know me really well!');
+}
+else {
+  alert('Awww, that\'s not the right answer :()');
+}
