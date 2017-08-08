10867e2c8f8d70a7ad42f0e087f1689e409bad19
Shannon Dillon Guha <sedillon93@gmail.com>
2017-08-08 15:00:10 -0700

    add console.log messages for question answers

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 var seattle = prompt('I was born in Washington state').toLowerCase();
 console.log('I was born in WA state');
+console.log('false');
 if (seattle === 'y' || seattle === 'yes') {
   alert('Good try! But the answer is no');
 }
@@ -11,6 +12,7 @@ else {
 
 var major = prompt('In college I majored in chemistry').toLowerCase();
 console.log('I majored in chemistry');
+console.log('false');
 if (major === 'y' || major === 'yes') {
   alert('Good try! But that isn\'t the right answer');
 }
@@ -20,6 +22,7 @@ else {
 
 var german = prompt('I spent five years studying German in school').toLowerCase();
 console.log('I studied German for 5 years');
+console.log('true');
 if (german === 'y' || german === 'yes') {
   alert('High five! You got it!');
 }
@@ -29,6 +32,7 @@ else {
 
 var college = prompt('I went to Colgate University in central New York').toLowerCase();
 console.log('I went to Colgate University');
+console.log('true');
 if (college === 'y' || college === 'yes') {
   alert('Well done! You\'re doing really well');
 }
@@ -38,9 +42,10 @@ else {
 
 var scuba = prompt('Scuba diving is one of my favorite activities');
 console.log('Scuba diving is my favorite activity');
+console.log('true');
 if (scuba === 'y' || scuba === 'yes') {
   alert('Wow! You know me really well!');
 }
 else {
-  alert('Awww, that\'s not the right answer :()');
+  alert('Awww, that\'s not the right answer :(');
 }
