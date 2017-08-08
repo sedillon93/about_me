d9d2ec8e2d2dce723d6978ce5427f4f83d4a5113
Shannon Dillon Guha <sedillon93@gmail.com>
2017-08-08 15:03:40 -0700

    make console.log messages more descriptive

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 'use: strict';
 
 var seattle = prompt('I was born in Washington state').toLowerCase();
-console.log('I was born in WA state');
-console.log('false');
+console.log('Q1: I was born in WA state');
+console.log('A1: false');
 if (seattle === 'y' || seattle === 'yes') {
   alert('Good try! But the answer is no');
 }
@@ -11,8 +11,8 @@ else {
 }
 
 var major = prompt('In college I majored in chemistry').toLowerCase();
-console.log('I majored in chemistry');
-console.log('false');
+console.log('Q2: I majored in chemistry');
+console.log('A2: false');
 if (major === 'y' || major === 'yes') {
   alert('Good try! But that isn\'t the right answer');
 }
@@ -21,8 +21,8 @@ else {
 }
 
 var german = prompt('I spent five years studying German in school').toLowerCase();
-console.log('I studied German for 5 years');
-console.log('true');
+console.log('Q3: I studied German for 5 years');
+console.log('A3: true');
 if (german === 'y' || german === 'yes') {
   alert('High five! You got it!');
 }
@@ -31,8 +31,8 @@ else {
 }
 
 var college = prompt('I went to Colgate University in central New York').toLowerCase();
-console.log('I went to Colgate University');
-console.log('true');
+console.log('Q4: I went to Colgate University');
+console.log('A4: true');
 if (college === 'y' || college === 'yes') {
   alert('Well done! You\'re doing really well');
 }
@@ -40,9 +40,9 @@ else {
   alert('Sorry, you missed that one');
 }
 
-var scuba = prompt('Scuba diving is one of my favorite activities');
-console.log('Scuba diving is my favorite activity');
-console.log('true');
+var scuba = prompt('Scuba diving is one of my favorite activities').toLowerCase();
+console.log('Q5: Scuba diving is my favorite activity');
+console.log('A5: true');
 if (scuba === 'y' || scuba === 'yes') {
   alert('Wow! You know me really well!');
 }
