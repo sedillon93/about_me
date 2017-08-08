2f37bcbd6674f7361acc68f0fdb54dc99a7306b2
Shannon Dillon Guha <sedillon93@gmail.com>
2017-08-08 16:40:00 -0700

    make console.log statements more accurate by replacing true/false with yes/no

--- app.js
+++ app.js
@@ -2,11 +2,13 @@
 
 var name = prompt('Hello there! Glad you stopped by today. What\'s your name?');
 var playGame = prompt('Hello ' + name + '!' + ' Would you like to get to know me a little better through my guessing game? Type "y" or "yes" for yes or "n" or "no" for no.').toLowerCase();
+console.log('Would you like to get to know me? Enter yes or no');
 if (playGame === 'y' || playGame === 'yes') {
+  console.log('Yes, you would like to play my guessing game.');
   alert('Wonderful! Let\'s play :)');
   var seattle = prompt('I was born in Washington state').toLowerCase();
   console.log('Q1: I was born in WA state');
-  console.log('A1: false');
+  console.log('A1: No');
   if (seattle === 'y' || seattle === 'yes') {
     alert('Good try! But the answer is no');
   }
@@ -16,7 +18,7 @@ if (playGame === 'y' || playGame === 'yes') {
 
   var major = prompt('In college I majored in chemistry').toLowerCase();
   console.log('Q2: I majored in chemistry');
-  console.log('A2: false');
+  console.log('A2: No');
   if (major === 'y' || major === 'yes') {
     alert('Good try! But that isn\'t the right answer');
   }
@@ -26,7 +28,7 @@ if (playGame === 'y' || playGame === 'yes') {
 
   var german = prompt('I spent five years studying German in school').toLowerCase();
   console.log('Q3: I studied German for 5 years');
-  console.log('A3: true');
+  console.log('A3: Yes');
   if (german === 'y' || german === 'yes') {
     alert('High five! You got it!');
   }
@@ -36,7 +38,7 @@ if (playGame === 'y' || playGame === 'yes') {
 
   var college = prompt('I went to Colgate University in central New York').toLowerCase();
   console.log('Q4: I went to Colgate University');
-  console.log('A4: true');
+  console.log('A4: Yes');
   if (college === 'y' || college === 'yes') {
     alert('Well done! You\'re doing really well');
   }
@@ -46,7 +48,7 @@ if (playGame === 'y' || playGame === 'yes') {
 
   var scuba = prompt('Scuba diving is one of my favorite activities').toLowerCase();
   console.log('Q5: Scuba diving is my favorite activity');
-  console.log('A5: true');
+  console.log('A5: Yes');
   if (scuba === 'y' || scuba === 'yes') {
     alert('Wow! You know me really well!');
   }
@@ -57,4 +59,5 @@ if (playGame === 'y' || playGame === 'yes') {
 
 else {
   alert('That\'s too bad. Maybe another time.');
+  console.log('No, you do no want to play');
 }
