4555463b0964779863f91e412e022349fc29bfb5
Shannon Dillon Guha <sedillon93@gmail.com>
2017-08-08 14:06:48 -0700

    add console.log entries

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 'use:strict';
 
 var seattle = prompt('I was born in Washington state');
+consolge.log('I was born in WA state');
 var major = prompt('In college I majored in chemistry');
+console.log('I majored in chemistry');
 var german = prompt('I spent five years studying German in school');
+console.log('I studied German for 5 years');
 var college = prompt('I went to Colgate University in central New York');
+console.log('I went to Colgate University');
 var scuba = prompt('Scuba diving is one of my favorite activities');
+console.log('Scuba diving is my favorite activity');
